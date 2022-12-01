a2e869898c467f6ec4c117f425eb257ddbb6e9e3
cp457 <112082288+cp457@users.noreply.github.com>
2022-12-01 17:08:04 +0100

    Add files via upload

---
+++ House/src/House.java
@@ -0,0 +1,12 @@
+public class House {
+
+    public String address;
+    public int numberOfFloor;
+
+
+    public  House(){
+
+        System.out.println("A new house ad been created");
+    }
+
+}
---
+++ House/src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] args){
+
+        //House house1;
+       // House house2;
+        House house = new House();
+        House house2 = new House();
+
+
+        String address = house.address;
+        int floor = house.numberOfFloor;
+
+        String address2 = house2.address;
+        int floor2 = house2.numberOfFloor;
+
+
+    }
+}
